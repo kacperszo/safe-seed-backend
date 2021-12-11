dc18d0db5e41257f3cc54627a3000aa624a407a6
Kacper Szot <kacperszot1@gmail.com>
2021-12-11 19:32:48 +0100

    update user fix

--- src/users/dtos/update-user.dto.ts
+++ src/users/dtos/update-user.dto.ts
@@ -1,8 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { MinLength, IsNotEmpty, IsArray } from 'class-validator';
 export class UpdateUserDto {
-  @IsArray()
-  @ApiProperty()
+  @ApiProperty({ required: false })
   tags: string[];
   @ApiProperty({ required: false })
   bio: string;
