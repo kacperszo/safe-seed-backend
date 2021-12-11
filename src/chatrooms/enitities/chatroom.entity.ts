c08ed524509e75b9aa53960546d45991e6a46b6e
FunHB <marcel.krolczyk@gmail.com>
2021-12-11 19:55:10 +0100

    przeklęci frontendowcy

--- src/chatrooms/enitities/chatroom.entity.ts
+++ src/chatrooms/enitities/chatroom.entity.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { User } from 'src/users/entities/user.entity';
 import {
   Column,
@@ -12,13 +13,17 @@ import { Message } from './message.entity';
 
 @Entity()
 export class Chatroom {
+  @ApiProperty()
   @PrimaryGeneratedColumn('uuid')
   id: string;
+  @ApiProperty()
   @ManyToMany(() => User, (user) => user.chatrooms, {})
   @JoinTable({ name: 'chatroom_user' })
   users: User[];
+  @ApiProperty()
   @OneToMany(() => Message, (message) => message.chatroom, { eager: true })
   messages: Message[];
+  @ApiProperty()
   @Column({ default: true, type: 'boolean', nullable: true })
   active: boolean;
 }
--- src/chatrooms/enitities/message.entity.ts
+++ src/chatrooms/enitities/message.entity.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { User } from 'src/users/entities/user.entity';
 import {
   Column,
@@ -11,12 +12,15 @@ import { Chatroom } from './chatroom.entity';
 //message entity
 @Entity()
 export class Message {
+  @ApiProperty()
   @PrimaryGeneratedColumn('uuid')
   id: string;
   @ManyToOne(() => User, (user) => user.messages)
   author: User;
+  @ApiProperty()
   @Column()
   message: string;
+  @ApiProperty()
   @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
   createdAt: Date;
   @ManyToOne(() => Chatroom, (chatroom) => chatroom.messages)
